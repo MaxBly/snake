fcbcca54487c63be0406271b0bb09bb16f570bd4
maxbly <max_bellamy@live.Fr>
2018-12-07 17:53:10 +0100

    git.help

---
+++ git.sh
@@ -0,0 +1,6 @@
+git pull #pour recuperer les fichiers, mieux vaut le faire avant de faire une maj
+git add # <fichiers> pour ajouter les fichiers au repos
+git commit -m "<message>" # idique la "maj"
+git push #pour mettre les fichiers en ligne
+git status #etat actuel des fichiers, modification...
+git diff [<fichiers>] #differences des fichiers en sur github et sur ton pc
