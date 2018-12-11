ac63d795e67e5ecd0ca6262e4c94026dd0f3bff2
maxbly <max_bellamy@live.Fr>
2018-12-11 11:29:32 +0100

    v1 comments

--- game.c
+++ game.c
@@ -5,13 +5,14 @@
 #include "game.h"
 
 void ggrid(int w, int h, int c, int r, _c color) {
-    gchoose(color);
-    for(int i = 1; i <= c; i++) {
-        gseg(w/c*i, 0, w/c*i, h);
+    gchoose(color); // on choisit la couleur
+    for(int i = 1; i <= c; i++) { // pour chacune des colones c;
+        gseg(w/c*i, 0, w/c*i, h); //on divise la largeur de la fenetre par le nb de colone (GRID) que l'on multiplie par la colone désirer
+
     }
     for (int j = 1; j <= r; j++) {
         gseg(0, h/r*j, w, h/r*j);
     }
 }
 
-int ggetCoords(int wh, int grid, int k) { return wh/(wh/grid)*k; }
+int ggetCoords(int wh, int grid, int k) { return wh/(wh/grid)*k; } // permet de recuperer les coodronné graphique a partir de la HEIGT/WIDTH, du coef GRID et de la case k
--- main.c
+++ main.c
@@ -4,9 +4,9 @@
 #include "functions.h"
 #include "game.h"
 
-#define     WIDTH   600
-#define     HEIGHT  450
-#define     GRID    15
+#define     WIDTH   600 // largeur de la fenetre
+#define     HEIGHT  450 // hauteur
+#define     GRID    15  // taille de la grille (unité arbitraire / quotient)
 
 #define     DEBUG
 
@@ -15,19 +15,19 @@ int main(void) {
     ginitWindow(100,100,WIDTH,HEIGHT);
     gclear(grgb(0,0,0));
 
-    int running = 1;
-    int x = 0, y = 0,
-        xx = 0, yy = 0;
+    int running = 1; // tant que running est a 1 le programme continue
+    int x = 0, y = 0, //x, y correspodent a la grille 
+        xx = 0, yy = 0; // xx, yy correspondent aux coordonnée de pixel
     //gchoose(grgb(255,0,0));
 
     while (running) {
-        gscreen(1);
-        gclear(grgb(0,0,0));
-        ggrid(WIDTH, HEIGHT, WIDTH/GRID, HEIGHT/GRID, grgb(255,255,255));
-        gchoose(grgb(255,0,0));
-        xx = ggetCoords(WIDTH, GRID, x);
+        gscreen(1); //basculage sur l'ecran fictif
+        gclear(grgb(0,0,0)); //clear l'ecran
+        ggrid(WIDTH, HEIGHT, WIDTH/GRID, HEIGHT/GRID, grgb(255,255,255)); // afficher la grille
+        gchoose(grgb(255,0,0)); // choisir la couleur rouge
+        xx = ggetCoords(WIDTH, GRID, x); //connaitre la position graphique de la case x
         yy = ggetCoords(HEIGHT, GRID, y);
-        gfillRect(xx, yy, GRID, GRID);
+        gfillRect(xx, yy, GRID, GRID); //afficher un carré rouge aux positions x, y
         #ifdef DEBUG
             printf("[%d|%d][%d|%d]\n", x, xx, y, yy);
         #endif /* DEBUG */
@@ -35,18 +35,18 @@ int main(void) {
 
 
 
-        if (gdoKey()) {
-            switch(ggetKey()) {
+        if (gdoKey()) { // si une touche est dispo
+            switch(ggetKey()) { //on teste la touche dispo, les variable XK_... sont definit dans <graph.h>
                 case XK_Escape: running = 0; break;
-                case XK_Left: if (x>0) x--; break;
-                case XK_Right: if (x<WIDTH/GRID-1) x++; break;
-                case XK_Down: if (y<HEIGHT/GRID-1) y++; break;
+                case XK_Left: if (x>0) x--; break;              //fleche gauche diminue x, si il est > a 0, pour ne pas sortir de la fenetre
+                case XK_Right: if (x<WIDTH/GRID-1) x++; break;  //fleche droite augment x si il est inférieut a la taille de la fenetre diviser par le coef de la grille, pour pas sortir
+                case XK_Down: if (y<HEIGHT/GRID-1) y++; break;  //meme principe pour le haut le bas
                 case XK_Up: if (y>0) y--; break;
             }
 
         }
 
-        gcopy(1,0,0,0,WIDTH,HEIGHT,0,0);
+        gcopy(1,0,0,0,WIDTH,HEIGHT,0,0);// bascule l'ecran fictif sur l'ecran affiché
     }
 
     return 0;
