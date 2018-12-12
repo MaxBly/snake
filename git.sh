420b057937f9cfc7dc6dd523b29a66b2fe207623
maxbly <max_bellamy@live.Fr>
2018-12-12 16:20:28 +0100

    clone

--- git.sh
+++ git.sh
@@ -4,3 +4,4 @@ git commit -m "<message>" # idique la "maj"
 git push #pour mettre les fichiers en ligne
 git status #etat actuel des fichiers, modification...
 git diff [<fichiers>] #differences des fichiers en sur github et sur ton pc
+git clone https://github.com/MaxBly/snake.git #pou recuperer les fichier depuis github (installation)
