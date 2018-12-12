be24f3622a6fd78c6d0f249258305284ddb2b050
maxbly <max_bellamy@live.Fr>
2018-12-12 17:21:02 +0100

    gitsh

--- git.sh
+++ git.sh
@@ -5,3 +5,7 @@ git push #pour mettre les fichiers en ligne
 git status #etat actuel des fichiers, modification...
 git diff [<fichiers>] #differences des fichiers en sur github et sur ton pc
 git clone https://github.com/MaxBly/snake.git #pou recuperer les fichier depuis github (installation)
+git checkout <branch> #pour changer de branch
+git branch <branch> #pour creer une branch
+git branch -d <branch> #pour supprimer une branch
+git merge <branch> #pour merger une branch sur la branche actuelle
